5c3a694ee60f73c778a8fa8045f11f3f04df4755
dlwxin <dlwxin@outlook.com>
2022-10-03 18:14:24 +0800

    update HelloWorldController.java

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -13,10 +14,17 @@ import java.util.UUID;
  */
 @CrossOrigin
 @RestController
+@RequestMapping("/api")
 public class HelloWorldController {
 
     @GetMapping("/")
     public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hash")
+    public String hash(){
         return UUID.randomUUID().toString();
     }
+    
 }
